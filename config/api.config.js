11e65516f03230e355bb8ed3140acdeae84b8f81
inscripoem <changxuben@gmail.com>
2025-01-21 15:42:53 +0800

    chore(config): add custom settings for huaxiaoke

--- config/api.config.js
+++ config/api.config.js
@@ -10,8 +10,8 @@
 module.exports = {
   // The clientId and clientSecret are used to authenticate the user with Microsoft Graph API using OAuth. You would
   // not need to change anything here if you can authenticate with your personal Microsoft account with OneDrive International.
-  clientId: 'd87bcc39-1750-4ca0-ad54-f8d0efbb2735',
-  obfuscatedClientSecret: 'U2FsdGVkX1830zo3/pFDqaBCVBb37iLw3WnBDWGF9GIB2f4apzv0roemp8Y+iIxI3Ih5ecyukqELQEGzZlYiWg==',
+  clientId: '6d179718-e57d-47fa-b570-dbafe3cddfe4',
+  obfuscatedClientSecret: 'U2FsdGVkX19p/3tYtR3yd0kYyS9bLhxGwc7twrl+RXhcz8/WJvHk670idfdiZCNiW8pzm99YKlSaBO+MAhgIAw==',
 
   // The redirectUri is the URL that the user will be redirected to after they have authenticated with Microsoft Graph API.
   // Likewise, you would not need to change redirectUri if you are using your personal Microsoft account with OneDrive International.
